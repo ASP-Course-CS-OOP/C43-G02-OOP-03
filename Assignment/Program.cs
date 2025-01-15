00970cf992ce931ac3e94ee4caff7daca326b67b
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-01-15 23:03:37 +0200

    Update Q1.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -14,9 +14,42 @@
 
             #region Q1. Design and implement a Class for the employees in a company:
 
-            //Employee employee = new Employee(1001, "Eslam Elsaadany", (SecurityLevel)15, 10_000, "1/1/2025", Gender.Male);
+            //bool IdParsed;
+            //int id;
+            //do
+            //{
+            //    Console.Write("Enter The Id: ");
+            //    IdParsed = int.TryParse(Console.ReadLine(), out id); 
+            //} while (!IdParsed);
 
-            //Console.WriteLine(employee);
+            //Console.Write("Enter The Name: " );
+            //string name = Console.ReadLine()!;
+
+            //Console.Write("Enter Security Level: ");
+            //SecurityLevel secLev = (SecurityLevel)int.Parse(Console.ReadLine()!);
+
+            //bool SalaryParsed;
+            //decimal salary;
+            //do
+            //{
+            //    Console.Write("Enter Salary: ");
+            //    SalaryParsed = decimal.TryParse(Console.ReadLine(), out salary); 
+            //} while (!SalaryParsed);
+
+            //Console.Write("Enter Hiring Date: ");
+            //string hiringDate = Console.ReadLine()!;
+
+            //bool GenderParsed;
+            //Gender gender;
+            //do
+            //{
+            //    Console.Write("Enter Gender: ");
+            //    GenderParsed = Enum.TryParse<Gender>(Console.ReadLine(), true, out gender); 
+            //} while (!GenderParsed);
+
+            //Employee employee = new Employee(id, name, secLev, salary, hiringDate, gender);
+
+            //Console.WriteLine($"\n{employee}");
 
             #endregion
 
