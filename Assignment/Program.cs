312991aa64a8b2992355db7d05dd2457665d7b70
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-01-16 18:32:39 +0200

    -Handle if user not enter any thing in name or enter white space -Correct While condition on hiring date attributes day(1 - 31) - month(1 - 12) - year (<=2025)

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -84,96 +84,100 @@ namespace Assignment
 
             #region Q3.Create an array of Employees with size three a DBA, Guest and the third one is security officer who have full permissions. (Employee [] EmpArr;)
 
-            //Employee[] empArr = new Employee[3];
-            //HiringDate[] hiringDate = new HiringDate[3];
-            //if (empArr?.Length >= 0)
-            //{
-            //    for (int i = 0; i < empArr.Length; i++)
-            //    {
-            //        Console.WriteLine($"\nEnter Data Of Employee {i + 1}=>\n");
+            Employee[] empArr = new Employee[3];
+            HiringDate[] hiringDate = new HiringDate[3];
+            if (empArr?.Length >= 0)
+            {
+                for (int i = 0; i < empArr.Length; i++)
+                {
+                    Console.WriteLine($"\nEnter Data Of Employee {i + 1}=>\n");
 
-            //        int id;
-            //        bool idParsed;
-            //        do
-            //        {
-            //            Console.Write("Id: ");
-            //            idParsed = int.TryParse(Console.ReadLine(), out id);
-            //        } while (!idParsed);
+                    int id;
+                    bool idParsed;
+                    do
+                    {
+                        Console.Write("Id: ");
+                        idParsed = int.TryParse(Console.ReadLine(), out id);
+                    } while (!idParsed);
 
-            //        Console.Write("Name: ");
-            //        string name = Console.ReadLine()!;
+                    string name;
+                    do
+                    {
+                        Console.Write("Name: ");
+                        name = Console.ReadLine()!; 
+                    } while (string.IsNullOrWhiteSpace(name));
 
-            //        Console.Write("Security Level: ");
-            //        SecurityLevel secLev = (SecurityLevel)int.Parse(Console.ReadLine()!);
+                    Console.Write("Security Level: ");
+                    SecurityLevel secLev = (SecurityLevel)int.Parse(Console.ReadLine()!);
 
-            //        bool SalaryParsed;
-            //        decimal salary;
-            //        do
-            //        {
-            //            Console.Write("Salary: ");
-            //            SalaryParsed = decimal.TryParse(Console.ReadLine(), out salary);
-            //        } while (!SalaryParsed);
+                    bool SalaryParsed;
+                    decimal salary;
+                    do
+                    {
+                        Console.Write("Salary: ");
+                        SalaryParsed = decimal.TryParse(Console.ReadLine(), out salary);
+                    } while (!SalaryParsed || salary<5000 || salary>100_000);
 
-            //        int day, month, year;
-            //        bool dayParsed, monthParsed, yearParsed;
+                    int day, month, year;
+                    bool dayParsed, monthParsed, yearParsed;
 
-            //        Console.WriteLine($"Hiring Date of Employee {i + 1}: ");
-            //        do
-            //        {
-            //            Console.Write("Day: ");
-            //            dayParsed = int.TryParse(Console.ReadLine(), out day);
-            //        } while (!dayParsed);
-            //        do
-            //        {
-            //            Console.Write("Month: ");
-            //            monthParsed = int.TryParse(Console.ReadLine(), out month);
-            //        } while (!dayParsed);
-            //        do
-            //        {
-            //            Console.Write("Year: ");
-            //            yearParsed = int.TryParse(Console.ReadLine(), out year);
-            //        } while (!dayParsed);
-            //        hiringDate[i] = new HiringDate(day, month, year);
+                    Console.WriteLine($"Hiring Date of Employee {i + 1}: ");
+                    do
+                    {
+                        Console.Write("Day: ");
+                        dayParsed = int.TryParse(Console.ReadLine(), out day);
+                    } while (!dayParsed || day>31 || day<1);
+                    do
+                    {
+                        Console.Write("Month: ");
+                        monthParsed = int.TryParse(Console.ReadLine(), out month);
+                    } while (!monthParsed || month>12 || month<1);
+                    do
+                    {
+                        Console.Write("Year: ");
+                        yearParsed = int.TryParse(Console.ReadLine(), out year);
+                    } while (!yearParsed || year>2025);
+                    hiringDate[i] = new HiringDate(day, month, year);
 
-            //        bool GenderParsed;
-            //        Gender gender;
-            //        do
-            //        {
-            //            Console.Write("Gender: ");
-            //            GenderParsed = Enum.TryParse<Gender>(Console.ReadLine(), true, out gender);
-            //        } while (!GenderParsed);
+                    bool GenderParsed;
+                    Gender gender;
+                    do
+                    {
+                        Console.Write("Gender: ");
+                        GenderParsed = Enum.TryParse<Gender>(Console.ReadLine(), true, out gender);
+                    } while (!GenderParsed);
 
-            //        empArr[i] = new Employee(id, name, secLev, salary, hiringDate[i], gender);
-            //    }
-            //}
+                    empArr[i] = new Employee(id, name, secLev, salary, hiringDate[i], gender);
+                }
+            }
 
-            //Console.WriteLine("\nEmployees Before sorting By Hiring Date:");
-            //foreach (Employee employee in empArr!)
-            //{
-            //    Console.WriteLine(employee);
-            //}
+            Console.WriteLine("\nEmployees Before sorting By Hiring Date:");
+            foreach (Employee employee in empArr!)
+            {
+                Console.WriteLine(employee);
+            }
 
             #endregion
 
             #region Q4.Sort the employees based on their hire date then Print the sorted array
 
-            //int boxingUnboxingCount = 0;
+            int boxingUnboxingCount = 0;
 
-            //Array.Sort(empArr, (e1, e2) =>
-            //{
-            //    boxingUnboxingCount++;
-            //    DateTime hireDate1 = new DateTime(e1.HireDate.Year, e1.HireDate.Month, e1.HireDate.Day);
-            //    DateTime hireDate2 = new DateTime(e2.HireDate.Year, e2.HireDate.Month, e2.HireDate.Day);
-            //    return hireDate1.CompareTo(hireDate2);
-            //});
+            Array.Sort(empArr, (e1, e2) =>
+            {
+                boxingUnboxingCount++;
+                DateTime hireDate1 = new DateTime(e1.HireDate.Year, e1.HireDate.Month, e1.HireDate.Day);
+                DateTime hireDate2 = new DateTime(e2.HireDate.Year, e2.HireDate.Month, e2.HireDate.Day);
+                return hireDate1.CompareTo(hireDate2);
+            });
 
-            //Console.WriteLine("\nEmployees After Sorting By Hire Date:");
-            //foreach (Employee employee in empArr)
-            //{
-            //    Console.WriteLine(employee);
-            //}
+            Console.WriteLine("\nEmployees After Sorting By Hire Date:");
+            foreach (Employee employee in empArr)
+            {
+                Console.WriteLine(employee);
+            }
 
-            //Console.WriteLine($"\nNumber of boxing & unboxing: {boxingUnboxingCount}");
+            Console.WriteLine($"\nNumber of boxing & unboxing: {boxingUnboxingCount}");
 
             #endregion
 
