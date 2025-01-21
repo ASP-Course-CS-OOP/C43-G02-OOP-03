4041a87a4cb92119b1d7d3db72cef6b7316a38fb
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-01-22 01:11:49 +0200

    Q01 - Write a class named Calculator that contains a method named Add. Overload the Add method to:

---
+++ AhmedNasrAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AhmedNasrAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part01
+
+            #region Q01 - Write a class named Calculator that contains a method named Add. Overload the Add method to:
+
+            //int Add2Int = Calculator.Add(1, 2);
+            //Console.WriteLine(Add2Int);// 3
+
+            //int Add3Int = Calculator.Add(1, 2,5);
+            //Console.WriteLine(Add3Int);// 8
+
+
+            //double Add2Double = Calculator.Add(1.7, 2.9);
+            //Console.WriteLine(Add2Double);// 4.6
+
+            #endregion
+
+            #endregion
+        }
+    }
+}
